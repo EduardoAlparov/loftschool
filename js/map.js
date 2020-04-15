439b198b86668e2f73cc5529aba290275bc89c56
Eduard Alparov <workman133@yandex.ru>
2020-04-15 21:28:07 +0300

    last tz without gulp

--- js/map.js
+++ js/map.js
@@ -17,7 +17,7 @@ const init = () => {
   const myCollection = new ymaps.GeoObjectCollection({}, {
     draggable: false,
     iconLayout: 'default#image',
-    iconImageHref: "../img/map/marker.png", 
+    iconImageHref: "./img/map/marker.png", 
     iconImageSize: [46, 57],
     iconImageOffset: [-35, -52]
   });
